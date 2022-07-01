5225489b7f9f9705dcdad8045c6d3089669efbac
Josh Simmons MBP <jcpsimmons@gmail.com>
2022-07-01 15:52:22 -0700

    error handling

--- main.ts
+++ main.ts
@@ -13,10 +13,15 @@ const fetchFromGiphy = async (
   }/search?api_key=${API_KEY}&q=${search}&limit=25`;
   const resp = await fetch(url);
   const { data } = await resp.json();
-  const gifUrls = data.map((img: { [key: string]: any }) => [
-    img.images.original.url.split("?")[0],
-    img.title.split(" ").join("-"),
-  ]);
+  const gifUrls = data
+    .map((img: { [key: string]: any }) => [
+      img?.images?.original?.url?.split("?")?.[0],
+      img?.title?.split(" ")?.join("-"),
+    ])
+    .filter(
+      (item: (string | undefined)[]) =>
+        item[0] !== undefined && item[1] !== undefined
+    );
   return gifUrls;
 };
 
@@ -46,15 +51,26 @@ const main = async () => {
     console.log("\n\n\n");
     console.log(`fetching ${query} as ${isSticker ? "sticker" : "gif"}...`);
     console.log("\n");
-    const gifData = await fetchFromGiphy(query, isSticker);
+    let gifData;
+    try {
+      gifData = await fetchFromGiphy(query, isSticker);
+    } catch (_error) {
+      console.error("error fetching data for " + query);
+      continue;
+    }
     Deno.mkdir(`./gifs/${query}`);
     for (const gif of gifData) {
-      const [url, title] = gif;
-      console.log(`downloading ${title}...`);
-      await download(url, {
-        dir: `${CWD}/gifs/${query}`,
-        file: `${title}.gif`,
-      });
+      try {
+        const [url, title] = gif;
+        console.log(`downloading ${title}...`);
+        await download(url, {
+          dir: `${CWD}/gifs/${query}`,
+          file: `${title}.gif`,
+        });
+      } catch (error) {
+        console.error("\x1b[41m", "error saving a gif");
+        console.log("\x1b[0m", " ");
+      }
     }
   }
 };
